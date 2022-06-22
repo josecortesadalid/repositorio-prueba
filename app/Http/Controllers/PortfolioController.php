105bffcbfea48f04433fcf9f1d366e3282ece4e8
josecortesadalid <jose.cortes@adalid.net>
2022-06-22 12:33:08 +0200

    probando Eloquent

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\DB;
@@ -12,7 +13,8 @@ class PortfolioController extends Controller
 
     public function index()
     {
-        $portfolio = DB::table('projects')->get();
+        // $portfolio = DB::table('projects')->get();
+        $portfolio = Project::orderBy('created_at', 'DESC')->get();
 
         return view('portafolio',compact('portfolio'));
     }
--- database/migrations/2022_06_22_070943_create_projects_table.php
+++ database/migrations/2022_06_22_070943_create_projects_table.php
@@ -15,6 +15,9 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->string('tecnologias');
             $table->timestamps();
         });
     }
